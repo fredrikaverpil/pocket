4b8d01792fcc4cfb19f1a2002e3c2ef30b9ece3b
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-01-15 18:26:18 +0100

    fix(tsqueryls): download prebuilt binaries instead of cargo install
    ts_query_ls has prebuilt releases on GitHub. Download those instead
    of requiring cargo to be installed on the system.

--- tools/tsqueryls/tsqueryls.go
+++ tools/tsqueryls/tsqueryls.go
@@ -4,6 +4,9 @@ package tsqueryls
 
 import (
 	"context"
+	"fmt"
+	"path/filepath"
+	"runtime"
 
 	"github.com/fredrikaverpil/pocket"
 )
@@ -11,17 +14,52 @@ import (
 // Name is the binary name for ts_query_ls.
 const Name = "ts_query_ls"
 
-// Repository is the git repository for ts_query_ls.
-const Repository = "https://github.com/ribru17/ts_query_ls"
-
-// Version is the git ref to install (branch, tag, or commit).
-// Note: ts_query_ls doesn't have versioned releases, so we use main.
-const Version = "main"
+// renovate: datasource=github-releases depName=ribru17/ts_query_ls
+const Version = "3.15.1"
 
 // Install ensures ts_query_ls is available.
-// Requires cargo to be installed on the system.
 var Install = pocket.Func("install:ts_query_ls", "install ts_query_ls", install).Hidden()
 
 func install(ctx context.Context) error {
-	return pocket.InstallCargoGit(ctx, Repository, Name, Version)
+	binDir := pocket.FromToolsDir("ts_query_ls", Version, "bin")
+	binaryName := pocket.BinaryName("ts_query_ls")
+	binaryPath := filepath.Join(binDir, binaryName)
+
+	platform := platformArch()
+	ext := "tar.gz"
+	if runtime.GOOS == pocket.Windows {
+		ext = "zip"
+	}
+
+	url := fmt.Sprintf(
+		"https://github.com/ribru17/ts_query_ls/releases/download/v%s/ts_query_ls-%s.%s",
+		Version, platform, ext,
+	)
+
+	return pocket.Download(ctx, url,
+		pocket.WithDestDir(binDir),
+		pocket.WithFormat(ext),
+		pocket.WithExtract(pocket.WithExtractFile(binaryName)),
+		pocket.WithSymlink(),
+		pocket.WithSkipIfExists(binaryPath),
+	)
+}
+
+func platformArch() string {
+	switch runtime.GOOS {
+	case pocket.Darwin:
+		if runtime.GOARCH == pocket.ARM64 {
+			return "aarch64-apple-darwin"
+		}
+		return "x86_64-apple-darwin"
+	case pocket.Linux:
+		if runtime.GOARCH == pocket.ARM64 {
+			return "aarch64-unknown-linux-gnu"
+		}
+		return "x86_64-unknown-linux-gnu"
+	case pocket.Windows:
+		return "x86_64-pc-windows-msvc"
+	default:
+		return fmt.Sprintf("%s-%s", runtime.GOARCH, runtime.GOOS)
+	}
 }
