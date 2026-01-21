f1541fa48c991b50b185f396d2c7dca4e12106ca
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-01-21 19:36:03 +0100

    chore: remove

--- internal/pok.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-:: Resolve shim directory to find .pocket
-set "SHIM_DIR=%~dp0"
-set "POCKET_DIR=%SHIM_DIR%../.pocket"
-set "TASK_SCOPE=internal"
-
-go run -C "%POCKET_DIR%" . %*
